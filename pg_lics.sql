43c12c39f323ed4e56a0d65deb09438b29b7a001
Dmytro Kazanzhy <dkazanzhy@gmail.com>
2021-04-15 15:59:17 +0300

    Update pg_lics.sql

--- pg_lics.sql
+++ pg_lics.sql
@@ -1,33 +1,43 @@
-/*
- * Longest increasinc consequent sequence
- * 
- * Run in ordered window on integer column
- * Returns last value of longest subsequence from 1 and increasing by 1.
+/* 
+ * Checks if INT in Longest Increasinc Consequent Sequence from 1 by 1
+ * Run in ordered window on integer column. 
  */
-
---------------------------------------------------------------------------------------------------------------------------------
-DROP FUNCTION IF EXISTS lics_row CASCADE;
-CREATE FUNCTION lics_row (prev_max INTEGER, curr INTEGER) RETURNS INTEGER AS $$
-    BEGIN
-        IF curr = (prev_max + 1) THEN RETURN curr;
-		ELSE RETURN prev_max;		
-        END IF;
+------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+DROP TYPE IF EXISTS corr_seq_state CASCADE;
+CREATE TYPE corr_seq_state AS (max_given INTEGER, correct_sequence bool);
+---------------------------------------------------------------------------------------------------
+DROP FUNCTION IF EXISTS corr_seq_row CASCADE;
+CREATE FUNCTION corr_seq_row (prev corr_seq_state, dose INTEGER) RETURNS corr_seq_state AS $$
+	BEGIN
+		IF dose = (prev.max_given + 1) THEN RETURN (dose, TRUE);
+		ELSEIF dose <= prev.max_given THEN RETURN (prev.max_given, TRUE);		
+		ELSE RETURN (prev.max_given, FALSE);
+		END IF;
     END;
 $$ LANGUAGE plpgsql; 
---------------------------------------------------------------------------------------------------------------------------------
-DROP AGGREGATE IF EXISTS lics (INTEGER) CASCADE;
-CREATE AGGREGATE lics (INTEGER) (
-    SFUNC = lics_row, 
-    STYPE = INTEGER, 
-    INITCOND = '0'
+---------------------------------------------------------------------------------------------------
+DROP FUNCTION IF EXISTS corr_seq_final CASCADE;
+CREATE FUNCTION corr_seq_final (curr amon.corr_seq_state) RETURNS BOOL AS $$
+    BEGIN
+		RETURN curr.correct_sequence;		
+    END;
+$$ LANGUAGE plpgsql; 
+---------------------------------------------------------------------------------------------------
+DROP AGGREGATE IF EXISTS corr_seq (INTEGER) CASCADE;
+CREATE AGGREGATE corr_seq (INTEGER) (
+	-- Run in EXPANDING window on integer column. Returns TRUE current int is in Increasing Subsequence from 1 by 1. 
+    SFUNC = corr_seq_row, 
+    STYPE = corr_seq_state, 
+    FINALFUNC = corr_seq_final,
+    INITCOND = '(0, FALSE)'
 );
---------------------------------------------------------------------------------------------------------------------------------
+------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+
 -- Test 1
 with temp_table_1 as (select unnest('{2,1,1,2,1,3,6,4}'::int[]) as seq
 	)
-SELECT *, lics(seq) over (order by seq)
-from temp_table_2
-
+SELECT *, corr_seq(seq) over (order by seq)
+from temp_table_1;
 ---------------------------------------------------------------------------------------
 -- Test 2
 with temp_table_2 as (
@@ -35,10 +45,7 @@ with temp_table_2 as (
 		unnest('{2,1,1,2,1,3,6,4}'::int[]) as vaccine_dose,
 		generate_series('2021-01-21', '2021-04-22', interval '13 days') as vaccine_date
 	)
-SELECT *, 
-	lics(vaccine_dose) over byVac as max_longest_seq,
-	vaccine_dose <= lics(vaccine_dose) over byVac as is_correct_sequence
+SELECT *, corr_seq(vaccine_dose) over byVac as is_correct_sequence
 from temp_table_2
-window byVac as (partition by vaccine_disease order by vaccine_date)
-
-
+window byVac as (partition by vaccine_disease order by vaccine_date);
+------------------------------------------------------------------------------------------------------------------------------------------------------------------------
