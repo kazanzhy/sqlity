e90b4de0a54cc1fae668935893097cb6ac660deb
Dmytro Kazanzhy <dkazanzhy@gmail.com>
2021-04-16 12:00:28 +0300

    Update pg_levenstein.sql

--- pg_levenstein.sql
+++ pg_levenstein.sql
@@ -6,43 +6,45 @@
 
 --------------------------------------------------------------------------------------------------------------------------------
 DROP FUNCTION IF EXISTS levenshtein CASCADE;
-CREATE FUNCTION levenshtein (TEXT, text, damerau bool default FALSE) RETURNS int STRICT AS $$
-	declare 
-		fst text := lower($1);
-		snd text := lower($2);
-		flen INT := length(fst);
-		slen INT := length(snd);
+CREATE FUNCTION levenshtein (TEXT, TEXT, damerau BOOL default FALSE) RETURNS INT STRICT AS $$
+	DECLARE 
+		fst TEXT := lower($1);
+		snd TEXT := lower($2);
+		flen INT := length(fst) + 1;
+		slen INT := length(snd) + 1;
 		D INT[][] := array_fill(0, array[flen, slen]);
 		cst INT; del INT; ins INT; sub INT;
-    BEGIN
-        for i in 1 .. flen loop 
-        	for j in 1 .. slen loop
-        		cst = (substring(fst,i,1) <> substring(snd,j,1))::int
-        		del := coalesce(D[i-1][j], i) + 1; -- deletion
-        		ins := coalesce(D[i][j-1], j) + 1; -- insertion
-        		sub := coalesce(D[i-1][j-1], 0) + cst; -- substitution
-        		D[i][j] := least(del, ins, sub);
-        		if damerau and i > 2 and j > 2 and substring(fst,i,1) = substring(snd,j-1,1) and substring(fst,i-1,1) = substring(snd,j,1) then
+	BEGIN
+		FOR i IN 1 .. flen LOOP 
+			D[i][1] := i-1;
+		END LOOP;
+		FOR j IN 1 .. slen LOOP
+			D[1][j] := j-1;
+		END LOOP;
+	    ---
+		FOR i IN 2 .. flen LOOP 
+			FOR j IN 2 .. slen LOOP
+				cst = (substring(fst,i-1,1) <> substring(snd,j-1,1))::int;
+				del := D[i-1][j] + 1; -- deletion
+				ins := D[i][j-1] + 1; -- insertion
+				sub := D[i-1][j-1] + cst; -- substitution
+				D[i][j] := least(del, ins, sub);
+				IF damerau and i > 3 and j > 3 and substring(fst,i-1,1) = substring(snd,j-2,1) and substring(fst,i-2,1) = substring(snd,j-1,1) THEN
 					D[i][j] := least(D[i][j], D[i-2][j-2] + 1);  -- transposition
-        		end if;
-        	end loop;
-	    END loop;
+				END IF;
+			END LOOP;
+		END LOOP;
 		RETURN D[flen][slen];		
-    END;
+	END;
 $$ LANGUAGE plpgsql;
 --------------------------------------------------------------------------------------------------------------------------------
                 
-
 -- Test
 with
 tmp as (select 'EDITING' as fst, 'DISTANCE' as snd
-		union
-		select 'Строка' as fst, 'собака' as snd
-		union 
-		select 'НСЗУ' as fst, 'НЗСУ' as snd
+		union select 'Строка', 'собака'
+		union select 'НСЗУ', 'НЗСУ'
+		union select 'book', 'back'
 	)
-select fst, snd, levenshtein(fst, snd), levenshtein(fst, snd, true)
+select fst, snd, levenshtein(fst, snd), levenshtein(fst, snd, true) as levenshtein_damerau
 from tmp
-
-
-
